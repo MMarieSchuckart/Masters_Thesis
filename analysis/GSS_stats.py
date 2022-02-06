c442e36a15621364b1de2cda2b2a1aa478050f87
Merle Marie Schuckart <40271978+MMarieSchuckart@users.noreply.github.com>
2022-02-06 18:54:05 +0100

    Update GSS_stats.py

--- analysis/GSS_stats.py
+++ analysis/GSS_stats.py
@@ -1,3 +1,4 @@
+
 """ Stats script for Merle's Master's Thesis
 
 Stats part for GSS data
@@ -7,27 +8,16 @@ Version 1: 13.01.2022
 
 """
 
+
 #%%
 
 #working_directory = "/Users/merle/Desktop/Masterarbeit/Master_Testdaten/"
-#gss_psd_tmin = 1
-#gss_psd_tmax = 4 
-#gss_psd_sfreq = 500 
-#gss_psd_fmin = 4 
-#gss_psd_fmax = 36 
-#gss_psd_n_overlap = 0 
-#gss_psd_n_per_seg = None 
-#gss_psd_n_jobs = 1
-#gss_psd_average = 'mean' 
-#gss_psd_window = 'hamming'
 
 # create function for running EEG stats script
 def GSS_stats(working_directory, 
-              gss_psd_tmin = 1, 
-              gss_psd_tmax = 4, 
-              gss_psd_sfreq = 500, 
+              gss_psd_sfreq = 80, 
               gss_psd_fmin = 4, 
-              gss_psd_fmax = 36, 
+              gss_psd_fmax = 12, 
               gss_psd_n_overlap = 0, 
               gss_psd_n_per_seg = None, 
               gss_psd_n_jobs = 1, 
@@ -54,6 +44,9 @@ def GSS_stats(working_directory,
     # necessary dependencies (numpy, scipy, and matplotlib).    
     import mne
 
+    # for working with pandas dataframes 
+    import pandas as pd
+    
     # for rounding frequency values
     import numpy as np
 
@@ -79,98 +72,166 @@ def GSS_stats(working_directory,
     # 2.1 set path to working directory
     os.chdir(working_directory)
     
-    # 2.2 get list of all files in the directory that end with "epo.fif"
-    file_list = glob.glob(working_directory + "gss_" + "*epo.fif")
-    
-    
-#%%
-    """ 3.1  Loop participants"""
-    for filename in file_list:
+    # 2.2 get list of all files in the directory that end with "_filtered_epo.fif"
+    file_list = glob.glob(working_directory + "gss_" + "*_filtered_epo.fif")
         
-        # get participant number from file name string
-        file = filename[-11:-8] # participant numbers can be 1 - 999
+    # create empty df for the results of the PSDs of all participants
+    gss_PSDs_all = pd.DataFrame(columns = ['block', 'sfb', 'sfc', 'feedback', 'epoch', 'power', 'frequency', 'ID'])
+
+   #%%  
+    """ 6. loop fif file names in file_list (aka loop participants): """
+    for file_idx in range(0, len(file_list)):
         
-        # correct the number if it has < 3 digits
-        if file[0] == "n":
-            file = file[-1]
-        elif file[0] == "t":
-            file = file[-2:len(file)]
-            
-        # 3.1 Read in the epoched data
-        epochs = mne.read_epochs("eeg_participant"+file+"_epo.fif")
-    
-        # save name of participant (yes I know I'm using the exact same value twice here)
-        part_nr = file
+        """ save participant number"""
+        # the participant numbers in file list are in the wrong order
+        # so get the number from the filename instead of the loop index number
+        participant = file_list[file_idx][-20:-17]
+        
+        # if participant number has < 3 digits, shorten the number
+        if participant[0] == "n":
+            participant = participant[-1]
+        elif participant[0] == "t":
+            participant = participant[-2:]        
+        
+        """ read in .fif file with the epochs and .csv file with the epoch conditions"""
+        epochs = mne.read_epochs("gss_participant" + str(participant) + "_filtered_epo.fif")
+        gss_epochs_conditions = pd.read_csv("gss_participant" + str(participant) + "_epo_conditions.csv", sep = ",")
+
+        # remove weird index column at position 0
+        gss_epochs_conditions.drop(columns = gss_epochs_conditions.columns[0], axis = 1, inplace = True)
+
          
 #%%
+
         """ loop epochs """
+        # placeholders for getting power peak and corresponding frequency
+        power = []
+        freq = []
+        
         for pick_epoch in range(0, epochs.__len__()):   
             
-            # get single epoch, crop to get time interval from second 1 - second 4 
-            single_epoch = epochs[pick_epoch].crop(tmin = gss_psd_tmin, tmax = gss_psd_tmax)
+            # get current epoch 
+            single_epoch = epochs[pick_epoch]
             
-            # save information converning the trial:
-            # number of epoch 
-            epoch_nr = pick_epoch
-            # feedback condition (= ov, oa, av)
-            feedback = single_epoch._metadata["feedback"].values[0]
-            # sfb condition (= scaling of feedback)
-            sfb = single_epoch._metadata["sfb"].values[0]
-            # sfc condition (= % of max. grip strength)
-            sfc = single_epoch._metadata["sfc"].values[0]
+            # check if the participant started pressing the sensor before trial onset + 1 sec
+            # get data from current epoch, count 0s at the beginning of the epoch
+            # --> if the beginning of the epoch looks like this: 
+            #     [0,0,0,0,0,0,0,0,0,0,2,3,0,0,0,2,3,4,5,6,3,4,5,9,...]
+            #                          ^
+            #                          |
+            #
+            # I assume the participant started using the sensor when the first 
+            # "real" values (see amateurish arrow above) were recorded, doesn't matter if 
+            # there are 0s afterwards. The reason is I can't know what happened there so 
+            # if there are 0s afterwards, maybe the person willingly decided not to use the sensor.
+            
+            # get data from epoch as list
+            epoch_data = list(single_epoch.get_data().flatten())
+            
+            # if there is at least 1 Zero at the beginning of the recording, 
+            # count how many there are and go to next epoch (aka exclude current epoch) 
+            # if there are more than 80 (aka 1s * 80Hz)
+        
+            if epoch_data[0] == 0:
+                # counter for zeros
+                nr_zeros = 0
+                # break while loop condition
+                count_on = True
+                # start counting at index 0
+                idx = 0
+                while count_on:
+                    # if current value is a 0,
+                    if epoch_data[idx] == 0:
+                        # add 1 to counter
+                        nr_zeros += 1
+                        # go to next value
+                        idx += 1
+                    # if curent value is not a 0...    
+                    else: 
+                        # stop counting
+                        count_on = False
+            # if the epochs doesn't start with a 0, we don't have to count            
+            else: nr_zeros = 0            
 
-            # set title of epoch
-            #title = "participant " + file + ", epoch " + str(pick_epoch) + " of " + str(len(epochs.picks))
-            
-            # plot psd (no clue which Method this is, the MNE docs don't want me to know this)
-            #single_epoch.plot_psd(fmin = 4, fmax = 12, spatial_colors = True)
-             
-            
-            
-            
-            
-            """ TO DO: Change the following part so it fits the GSS data """
-            # There are too many channels in the epoch object, this doesn't make sense.
-            
-            # get data for the current channel
-            channel_epoched_data = single_epoch._data[0]
-            
-            """compute power spectral density (PSD) analysis using Welch's Method """
-            # the following function returns... 
-            # ...the PSD values for each channel (--> saved in object "psds")... 
-            # ...for all frequencies (--> saved in object "freqs")... 
-            # ...for the current epoch
-            psds, freqs = mne.time_frequency.psd_array_welch(channel_epoched_data,
-                                                             sfreq = gss_psd_sfreq, 
-                                                             fmin = gss_psd_fmin, 
-                                                             fmax = gss_psd_fmax, 
-                                                             n_fft = len(channel_epoched_data), 
-                                                             n_overlap = gss_psd_n_overlap, 
-                                                             n_per_seg = gss_psd_n_per_seg, 
-                                                             n_jobs = gss_psd_n_jobs, 
-                                                             average = gss_psd_average, 
-                                                             window = gss_psd_window)
-            # round frequencies
-            freqs = np.round(freqs, 1)
-
-            """ loop frequencies """
-            for freq_val in range(0,len(freqs)-1):
-
-                # if freq value is an integer...
-                if freqs[freq_val].is_integer():
-                    # get corresponding value from psds array and add to df
-                    freq = freqs[freq_val] 
-                    psd_val = psds[freq_val]                  
- 
-                    # save nr of participant, nr of epoch, metadata,
-                    # nr of channel, frequency & corresponding power value 
-                    # as temporary 1 row df:
-                    tmp_df.loc[0] = [part_nr, epoch_nr, feedback, sfb, sfc, channel_nr, freq, psd_val]
-                    
-                    # append as new row to dataframe containing the values for all participants:
-                    power_vals_all = power_vals_all.append(tmp_df)
+            if nr_zeros >= 80:
+                print("Participant didn't react or reacted to slow\nin epoch " + 
+                      str(pick_epoch) + ", excluding this epoch now!" )
+                # don't compute PSD for this epoch, save None 
+                # instead as peak freq and power value:
+                power.append(None)
+                freq.append(None)
                 
-            # END loop frequencies    
+            # if everything's fine, compute PSD for current epoch and save results
+            else:
+            
+                # Settings:
+                            
+                # this is just for testing this part of the script, 
+                # I set these as arguments in the function call:
+                # gss_psd_sfreq = 80 
+                # gss_psd_fmin = 4 
+                # gss_psd_fmax = 12 
+                # gss_psd_n_overlap = 0 
+                # gss_psd_n_per_seg = None 
+                # gss_psd_n_jobs = 1
+                # gss_psd_average = 'mean' 
+                # gss_psd_window = 'hamming'
+
+
+                # plot psd (no clue which Method this is, the MNE docs don't want me to know this)
+                #single_epoch.plot_psd(fmin = 4, fmax = 12, spatial_colors = True)
+                 
+                
+                """compute power spectral density (PSD) analysis using Welch's Method """
+                
+                # the following function returns... 
+                # ...the PSD values for each channel (--> saved in object "psds")... 
+                # ...for all frequencies (--> saved in object "freqs")... 
+                # ...for the current epoch.
+                
+                # get data 
+                single_epoch = np.array(single_epoch.get_data().flatten())
+                
+                psds, freqs = mne.time_frequency.psd_array_welch(single_epoch,
+                                                                 sfreq = gss_psd_sfreq, 
+                                                                 fmin = gss_psd_fmin, 
+                                                                 fmax = gss_psd_fmax, 
+                                                                 n_fft = len(single_epoch), 
+                                                                 n_overlap = gss_psd_n_overlap, 
+                                                                 n_per_seg = gss_psd_n_per_seg, 
+                                                                 n_jobs = gss_psd_n_jobs, 
+                                                                 average = gss_psd_average, 
+                                                                 window = gss_psd_window)
+                
+                # round frequencies
+                freqs = np.round(freqs, 1)
+    
+                # turn psds array into list
+                psds = psds.tolist()
+    
+                # get highest power value and corresponding frequency
+                peak_power = max(psds)
+                peak_freq = freqs[psds.index(max(psds))]
+    
+                # save peak frequency & corresponding power value 
+                # as temporary 1 row df:
+                power.append(peak_power)
+                freq.append(peak_freq)
+                     
         # END loop epochs
-        print(" -- finished computing betas for participant " + str(file) + " -- ")
+        
+        # append freq and power to gss_epochs_conditions
+        gss_epochs_conditions["power"] = power
+        gss_epochs_conditions["frequency"] = freq
+        
+        # create list with participant identifier and append to gss_epochs_conditions as well
+        gss_epochs_conditions["ID"] = [participant] * len(gss_epochs_conditions)
+        
+        # append gss_epochs_conditions as new set of rows to df for all participants
+        gss_PSDs_all = gss_PSDs_all.append(gss_epochs_conditions)
+        
     # END loop participants
+    
+    
+    
+   
